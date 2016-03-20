1d5f59abc7632b02b9e644f2554100bab48f5bf8
Taylor Nycum <taylor.nycum@gmail.com>
2016-03-20 13:43:45 -0400

    Updated UI with current activity text and history listview.

--- app/src/main/java/com/example/jennahuston/activityrecognitiontry/MainActivity.java
+++ app/src/main/java/com/example/jennahuston/activityrecognitiontry/MainActivity.java
@@ -5,11 +5,8 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -25,15 +22,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         //Bind the service
         Intent myIntent2 = new Intent(this, BoundedService.class);
